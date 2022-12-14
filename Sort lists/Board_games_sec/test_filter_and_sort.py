f29f87b5831136270053b4e5b9ced415ea3fac2f
Bar <barkadosh@gmail.com>
2022-12-14 20:44:04 +0200

    141222

---
+++ Sort lists/Board_games_sec/test_filter_and_sort.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.event_firing_webdriver import EventFiringWebDriver
+from webdriver_manager.chrome import ChromeDriverManager
+import event_listener
+
+class TestSortBoardGames:
+    @classmethod
+    def setup_class(cls):
+        global driver
+        edriver = webdriver.Chrome(ChromeDriverManager().install())
+        driver = EventFiringWebDriver(edriver, event_listener.EventListener())
+        driver.get("https://www.miniaturemarket.com/")
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+
+    @classmethod
+    def teardown_class(cls):
+            driver.quit()
+
+    def test_sort_boardgames(self):
+        # Click on Board Games
+        driver.find_element(By.XPATH, "//a[@href='https://www.miniaturemarket.com/board-games.html']").click()
+        # Accept cookies
+        cookies = driver.find_element(By.CSS_SELECTOR, "button#onetrust-accept-btn-handler")
+        cookies.click()
+        # Filter by Age
+        driver.find_element(By.PARTIAL_LINK_TEXT, "3 and up").click()
+        # Sort by lowest price
+        driver.find_element(By.CSS_SELECTOR, ".sort-mode.control-set").click()
+        driver.find_element(By.CSS_SELECTOR, "[title='Price ($ - $$$)']").click()
+        # Check price on first product
+        product_1 = driver.find_elements(By.XPATH,"//span[text()='Our Price:']/following-sibling::span[@class='price']")[0].text
+        # Check price on second product
+        product_2 = driver.find_elements(By.XPATH,"//span[text()='Our Price:']/following-sibling::span[@class='price']")[1].text
+
+        # Check the first price is lower than the second price
+        assert float(product_1.replace('$', '')) <= float(product_2.replace('$', ''))
