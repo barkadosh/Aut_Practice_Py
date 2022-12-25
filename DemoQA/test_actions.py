adcb3cd347b2c5cd7403e95e73d9111d8ad23792
barkadosh <barkadosh@gmail.com>
2022-12-25 14:39:32 +0200

    211222

--- DemoQA/test_actions.py
+++ DemoQA/test_actions.py
@@ -26,47 +26,38 @@ class TestActionsChains:
         time.sleep(5)
         driver.quit()
 
-    def test_opentab(self):
+    @allure.title("Drag and drop")
+    @allure.description("This test a drag and drop in the app")
+    def test_drag_and_drop(self):
+        try:
+            self.step_drag_and_drop()
+            self.step_verify_dnd()
+        except:
+            self.attach_file()
+            pytest.fail("Test failed, see attached screen shot")
+
+    @allure.step("Open drag and drop page")
+    def step_go_to_dnd_app(self):
         driver.find_element(By.CSS_SELECTOR, "div.card.mt-4.top-card:nth-child(5)").click()
         droppable_menu = WebDriverWait(driver, 3).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "div:nth-child(5)>div>ul>li#item-3")))
         driver.execute_script("arguments[0].click();", droppable_menu)
+
+    @allure.step("Drag and drop")
+    def step_drag_and_drop(self):
         action = ActionChains(driver)
         draggable = driver.find_element(By.ID, "draggable")
         droppable = driver.find_element(By.ID, "droppable")
         action.drag_and_drop(draggable, droppable).perform()
 
+    @allure.step("Verify document dropped")
+    def step_verify_dnd(self):
+        droppable = driver.find_element(By.CSS_SELECTOR, "div.trash")
+        assert droppable.get_attribute("class") == "trash full"
 
-    # @allure.title("Drag and drop")
-    # @allure.description("This test a drag and drop in the app")
-    # def test_drag_and_drop(self):
-    #     try:
-    #         self.step_drag_and_drop()
-    #         self.step_verify_dnd()
-    #     except:
-    #         self.attach_file()
-    #         pytest.fail("Test failed, see attached screen shot")
-    #
-    # @allure.step("Open drag and drop page")
-    # def step_open_dnd_url(self):
-    #     driver.find_element(By.XPATH,"//div/h5[text()='Interactions']"
-    #                                  "/preceding-sibling::div[@class='card mt-4 top-card']").click()
-    #
-    # @allure.step("Drag and drop")
-    # def step_drag_and_drop(self):
-    #     action = ActionChains(driver)
-    #     draggable = driver.find_elements(By.CSS_SELECTOR, "img")[0]
-    #     droppable = driver.find_element(By.CSS_SELECTOR, "div.trash")
-    #     action.drag_and_drop(draggable, droppable).perform()
-    #
-    # @allure.step("Verify document dropped")
-    # def step_verify_dnd(self):
-    #     droppable = driver.find_element(By.CSS_SELECTOR, "div.trash")
-    #     assert droppable.get_attribute("class") == "trash full"
-    #
-    # def attach_file(self):
-    #     image = "./screen-shots/screen.png"
-    #     driver.get_screenshot_as_file(image)
-    #     allure.attach.file(image, attachment_type=allure.attachment_type.PNG)
+    def attach_file(self):
+        image = "./screen-shots/screen.png"
+        driver.get_screenshot_as_file(image)
+        allure.attach.file(image, attachment_type=allure.attachment_type.PNG)
 
 
