48b7c373b52150b3e8b1b0ec7f488ca994e12831
barkadosh <barkadosh@gmail.com>
2023-01-18 09:25:47 +0200

    11012023

--- Course_interview_questions/Woolovers/test_woolovers.py
+++ Course_interview_questions/Woolovers/test_woolovers.py
@@ -10,10 +10,9 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from TakeScreenShot import take_screenshot
 
 # Ex - https://drive.google.com/file/d/1Jw0W6z7PRwRc4NwonVybZY2txpkcFxoh/view
-# Apllitools - visual testing
+# Apllitools - visual testing - taking screenshots after changes, comparing the changes with past runs
 eyes = Eyes()
 
 
@@ -24,71 +23,39 @@ class TestWooLoversWeb:
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         driver.get("https://www.wooloverslondon.com/")
         driver.maximize_window()
-        driver.implicitly_wait(3)
-        eyes.api_key = 'rCfg7VUAMD1y9jG97L8H101bVw0yaF0RQuC2ZWo0q47pCs110'
+        driver.implicitly_wait(10)
+        #eyes.api_key =
 
     @classmethod
     def teardown_class(cls):
         driver.quit()
         eyes.abort()
 
-    @allure.title("TC 01 - Change currency")
-    @allure.description("Change the currency of the prices US Dollar")
+    @allure.title("TC 01 - Change filters")
+    @allure.description("Change the currency to $, gender to male and price range from low to high")
     def test_change_currency(self):
-        try:
-            self.step_close_popups()
-            self.step_change_currency()
-        except Exception as error:
-            print(error)
-            pytest.fail()
-        finally:
-            take_screenshot(driver)
-
-    @allure.step("Approve and close 2 popups")
-    def step_close_popups(self):
         driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
-        driver.find_element(By.XPATH, "//input[@value='המשך לקנות']").click()
-
-    @allure.step("Change the currency to US Dollar")
-    def step_change_currency(self):
-        eyes.open(driver, "Change currency", "Change currency on page to US Dollar")
-        driver.find_element(By.CLASS_NAME, "font-weight-bold").click()
+        driver.find_element(By.XPATH, "//input[@type='button']").click()
+        eyes.open(driver, "Change filters", "Change the currency to $, gender to male and price range from low to high")
+        driver.find_element(By.XPATH, "//a[@class='font-weight-bold']").click()
         eyes.check_window("Before changing the currency")
         driver.find_element(By.XPATH, "//img[@src='//gepi.global-e.com/content/images/flags/il.png']").click()
         WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.CLASS_NAME, "glPopupContent")))
         currency = Select(driver.find_element(By.ID, "gle_selectedCurrency"))
         currency.select_by_visible_text("US Dollar")
-        driver.find_element(By.XPATH, "//input[@value='שמירה']").click()
-        eyes.check_window("After changing the currency")
-        eyes.close()
-
-    @allure.title("TC 02 - Change gender")
-    @allure.description("Change the currency of the prices US Dollar")
-    def test_change_gender(self):
-        try:
-            self.step_close_popups()
-            self.step_change_gender()
-        except Exception as error:
-            print(error)
-            pytest.fail()
-        finally:
-            take_screenshot(driver)
-
-    @allure.step("Approve and close 2 popups")
-    def step_close_popups(self):
-        driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
-        driver.find_element(By.XPATH, "//input[@value='המשך לקנות']").click()
-
-    @allure.step("Change the gender to Male")
-    def step_change_gender(self):
-        driver.find_element(By.CLASS_NAME, "font-weight-bold").click()
+        driver.find_element(By.XPATH, "//input[@data-key='SavenClose']").click()
+        eyes.check_window("After changing the currency, Before changing the gender")
         driver.find_element(By.ID, "dd-Gender").click()
-        driver.find_element(By.XPATH, "//a[@data-filter-id='161']").click()
-        driver.find_element(By.XPATH, "//a[@data-filter-id='160']").click()
-        driver.find_element(By.CLASS_NAME, "btn btn-block filter-group__close-btn js-toggle-close-all]").click()
-
-
-
-
-
+        driver.find_element(By.CSS_SELECTOR, "div.d-lg-flex.filter-group__scroll>a[data-filter-id='161']").click()
+        driver.find_element(By.CSS_SELECTOR, "div.d-lg-flex.filter-group__scroll>a[data-filter-id='160']").click()
+        time.sleep(5)
+        driver.find_element(By.CSS_SELECTOR, "div.filter-group__close-all.hidden-xs.hidden-sm").click()
+        eyes.check_window("After changing the gender, Before changing price range")
+        driver.find_element(By.ID, "dd-Sort By").click()
+        driver.find_element(By.CSS_SELECTOR, "div.d-lg-flex.filter-group__scroll>a[data-filter-id='0']").click()
+        driver.find_element(By.CSS_SELECTOR, "div.d-lg-flex.filter-group__scroll>a["
+                                             "href='?f=t&filtered&sortby=1&gender=161,&show=24&category=0']").click()
+        driver.find_elements(By.XPATH, "//div[text()='Done']")[15].click()
+        eyes.check_window("After changing price range from low to high")
+        eyes.close()
